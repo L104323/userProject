9c0fd70956c16c47af1fcc803fbe4d24e4537a98
meKey <2911648919@qq.com>
2018-07-18 09:31:57 +0800

    第一次提交到github

---
+++ Car/src/car/car.java
@@ -0,0 +1,21 @@
+package car;
+
+public class car {
+	public static void main(String[] args){
+	c cc =new c();
+	cc.setName("fgvgbgb");
+	cc.name="nsfuiocn";
+	System.out.println(cc.getName());
+}
+}
+class c{
+String name;
+String data;
+String color;
+String getName(){
+   return name;
+}
+void setName(String _name){
+	name = _name;
+}
+}
